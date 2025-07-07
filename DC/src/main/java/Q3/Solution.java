d41c96b41fcddcd7a1e2fa67371cfae115abf88c
Renan Lacerda <renanrodlacerda@gmail.com>
2025-07-07 03:50:33 -0300

    solucao Merge k Sorted Lists

---
+++ DC/src/main/java/Q3/Solution.java
@@ -0,0 +1,56 @@
+package Q3;
+
+public class Solution {
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists == null || lists.length == 0) {
+            return null;
+        }
+        return merge(lists, 0, lists.length - 1);
+    }
+
+    private ListNode merge(ListNode[] lists, int start, int end) {
+        if (start == end) return lists[start];
+        if (start > end) return null;
+
+        int mid = start + (end - start) / 2;
+
+        ListNode left = merge(lists, start, mid);
+        ListNode right = merge(lists, mid + 1, end);
+
+        return mergeTwoLists(left, right);
+    }
+
+    private ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+
+        ListNode dummyHead = new ListNode(0);
+        ListNode current = dummyHead;
+
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                current.next = l1;
+                l1 = l1.next;
+            } else {
+                current.next = l2;
+                l2 = l2.next;
+            }
+            current = current.next;
+        }
+
+        if (l1 != null) {
+            current.next = l1;
+        } else {
+            current.next = l2;
+        }
+
+        return dummyHead.next;
+    }
+}
